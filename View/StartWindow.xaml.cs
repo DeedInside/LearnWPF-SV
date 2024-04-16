a7d19c33f0c66b6be2f5c4d33946718f3c60c216
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-04-16 14:19:52 +0700

    Добавьте файлы проекта.

---
+++ View/StartWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Emit;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LearnWPF_SV.View
+{
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+         
+        }
+
+
+    }
+}
