6fb86b5a8708a634cb2ef6bac0456d10f6e20cdd
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-05-07 14:29:01 +0700

    07/05

--- View/StartWindow.xaml.cs
+++ View/StartWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Reflection.Emit;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace LearnWPF_SV.View
 {
@@ -14,5 +15,29 @@ namespace LearnWPF_SV.View
          
             MyLable.Content = "qweqweqwe";
         }
-    }
+
+		private void Copu(object sender, RoutedEventArgs e)
+		{
+            string str = box.Text;
+
+            lab.Content = str;  
+
+		}
+
+		private void CheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+            chek2.IsChecked = true;
+		}
+
+		private void RadioButton_Checked(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void RadioButton(object	 sender, RoutedEventArgs e)
+		{
+			RadioButton pressed = sender as RadioButton;
+			MessageBox.Show(pressed.Content.ToString());
+		}
+	}
 }
